0744234519e103735dd565fce5b641bf27ab6303
jenniffergon <j.gonzalezg2@uniandes.edu.co>
2023-04-28 23:31:33 -0500

    Crear componente lista cafe

---
+++ src/app/cafes/cafes-list/cafes-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cafes-list',
+  templateUrl: './cafes-list.component.html',
+  styleUrls: ['./cafes-list.component.css']
+})
+export class CafesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/cafes/cafes.module.ts
+++ src/app/cafes/cafes.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CafesComponent } from './cafes.component';
+import { CafesListComponent } from './cafes-list/cafes-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [CafesComponent]
+  exports: [CafesListComponent],
+  declarations: [CafesComponent, CafesListComponent]
 })
 export class CafesModule { }
