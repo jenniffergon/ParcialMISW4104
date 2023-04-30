4a88ac3387a1c4c8618c6fceeb8529ec374178ea
jenniffergon <j.gonzalezg2@uniandes.edu.co>
2023-04-30 00:55:13 -0500

    prueba del componente

--- src/app/cafes/cafes-list/cafes-list.component.spec.ts
+++ src/app/cafes/cafes-list/cafes-list.component.spec.ts
@@ -5,17 +5,19 @@ import { DebugElement } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { CafesListComponent } from './cafes-list.component';
+import { CafesService } from '../cafes.service';
+import { Cafe } from '../cafe';
 
 describe('CafesListComponent', () => {
   let component: CafesListComponent;
   let fixture: ComponentFixture<CafesListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-      HttpClientModule
-    ],
-      declarations: [ CafesListComponent ]
+      imports: [ HttpClientModule ],
+      declarations: [ CafesListComponent ],
+      providers: [ CafesService ]
     })
     .compileComponents();
   }));
@@ -23,10 +25,33 @@ describe('CafesListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CafesListComponent);
     component = fixture.componentInstance;
+
+    for(let i = 0; i < 3; i++) {
+      const cafe = new Cafe(
+        i,
+        "Café Especial "+i,
+        "blend",
+        "Fredonia, Antioquia",
+        "Chocolate negro, Caramelo",
+        1450,
+        "https://cdn.shopify.com/s/files/1/0272/2873/3504/products/cafe-especial-pristino-1-cafe-colombiano-f_720x.png"
+      );
+      component.cafe.push(cafe);
+    }
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Se crearon 3 filas de cafes ', () => {
+    expect(debug.queryAll(By.css('th.fila'))).toHaveSize(3)
+  });
+
+  it('Se crearon 3 filas más el encabezado ', () => {
+    expect(debug.queryAll(By.css('tr'))).toHaveSize(4)
   });
 });
